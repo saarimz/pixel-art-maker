dc0f0a596922a2f39aafc60c752c2dcc84c71e5a
Saarim Zaman <saarim.z@gmail.com>
2017-09-02 18:21:35 -0400

    finished up to do's

--- designs.js
+++ designs.js
@@ -1,52 +1,8 @@
-// Select color input
-// Select size input
-/*
-// When size is submitted by the user, call makeGrid()
-$(document).ready(function(){
-
-  	function makeGrid(height,width) {
-	
-	//clear grid
-	$("tbody").empty();
-
-	//function to create string to insert
-	let stringToInsert = function(height,width) {
-		let result = "";
-		for (let i = 0; i < height; i++) {
-	      result += "<tr>";
-	      for (let j = 0; j < width; j++) {
-	      	result += "<td></td>";
-	      }
-	      result += "</tr>";
-		}
-		return result;
-	};
-	//pass h x w to function to create string to append
-	$("table#pixel_canvas > tbody:last-child").append(stringToInsert(height,width));
-	}
-
-	//when submit is pressed
-    $("#submit").click(function(e){
-    	//prevent page from refreshing
-    	e.preventDefault();
-    	//create the grid
-    	let height = $("#input_height").val();
-    	let width = $("#input_width").val();
-    	makeGrid(height,width);
-
-    	//create listener for when cell is clicked
-    	$("td").click(function(){
-	    	$(this).css("background-color",$("#colorPicker").val());  	
-	    });
-    });
-
-});
-*/
 
 //defaults
 const tBody = document.createElement("tbody");
 document.getElementById("pixel_canvas").appendChild(tBody);
-
+let color = document.getElementById("colorPicker").value;
 
 //makeGrid
 function makeGrid(height,width){
@@ -76,6 +32,13 @@ document.getElementsByTagName("input")[2].addEventListener("click",function(e){
 	makeGrid(inputHeight,inputWidth);
 });
 
-//make cell event listener to change background color
-
 //make color picker event listener
+document.getElementById("colorPicker").addEventListener("change",function(e){
+	color = document.getElementById("colorPicker").value;
+});
+//make the td cells selectable / colorable
+document.getElementById("pixel_canvas").addEventListener("click",function(e){
+	e.target.style.backgroundColor = color;
+},false);
+
+
